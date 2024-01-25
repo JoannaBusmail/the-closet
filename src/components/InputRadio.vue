48b82a17846aee8573fe9da8b208045a34b82703
JoannaBusmail <joanna.busmail@gmail.com>
2024-01-25 15:45:05 +0100

    Input fixes and upload photo form

---
+++ src/components/InputRadio.vue
@@ -0,0 +1,45 @@
+<template>
+    <input
+        class="fixedStyles"
+        type="radio"
+        :id="id"
+        :checked="isChecked"
+        @change="$emit('update:modelValue', value)"
+    >
+    <label :for="id">{{ label }}</label>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+    modelValue: {
+        type: String,
+        required: true,
+    },
+    id: String,
+    label: String,
+    value: String
+})
+
+const isChecked = computed(() => props.modelValue === props.value)
+
+</script>
+
+
+<style scoped>
+.fixedStyles {
+    margin: 15px 10px;
+    border: 1px solid #a8a5a5;
+    border-radius: 10px;
+    font-family: 'Inter', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
+    font-size: 14px;
+    padding: 0 15px;
+    cursor: pointer;
+    width: 20px;
+    height: 20px;
+
+
+}
+</style>
+```
--- src/components/UploadPhoto.vue
+++ src/components/UploadPhoto.vue
@@ -1,26 +1,40 @@
 <template>
-    <div>Upload Photo</div>
-    <div>
+    <div class="upload-container">
         <form class="upload-form-container">
-            <label
-                for="inputFile"
-                class="custom-file-upload"
-            >
-                <span class="file-upload-text">Upload photo</span>
-            </label>
-            <input
-                class="input-file"
-                type="file"
-                id="inputFile"
-                accept=".jpg, .png, .jpeg"
-            >
-            <InputText
-                class="secondary"
-                type="text"
-                placeholder="Color"
-                v-model=color
-                @update:modelValue="$emit('update:colorInputValue', $event)"
-            />
+            <div class="input-container">
+                {{ color }}
+                <div class="file-text-input">
+                    <label
+                        for="inputFile"
+                        class="custom-file-upload"
+                    >
+                        <span class="file-upload-text">Upload photo</span>
+                    </label>
+                    <input
+                        class="input-file"
+                        type="file"
+                        id="inputFile"
+                        accept=".jpg, .png, .jpeg"
+                    />
+                    <InputText
+                        class="secondary"
+                        type="text"
+                        placeholder="Color"
+                        v-model="color"
+                    />
+                </div>
+                <div class="radio-input">
+                    <InputRadio
+                        v-for="(option, index) in radioOptions"
+                        v-model="selectedRadio"
+                        :key="index"
+                        :id="option.id"
+                        :label="option.label"
+                        :value="option.value"
+                    >{{ selectedRadio }}
+                    </InputRadio>
+                </div>
+            </div>
             <div class="button-container">
                 <Button
                     btnName="OK"
@@ -31,75 +45,86 @@
                     btnType="secondary"
                 ></Button>
             </div>
-
         </form>
-
     </div>
 </template>
-
+  
 <script setup>
 import Button from './Button.vue'
 import InputText from './InputText.vue'
-import { ref } from 'vue'
+import InputRadio from './InputRadio.vue'
+import { ref, watchEffect } from 'vue'
 
 const emit = defineEmits([ 'update:colorInputValue' ])
 
+
+const radioOptions = ref([
+    { id: 'casual', label: 'Casual', value: 'casual' },
+    { id: 'elegant', label: 'Elegant', value: 'elegant' },
+])
+
 const color = ref('')
+const selectedRadio = ref('')
+
+
+watchEffect(() =>
+{
+    console.log(selectedRadio.value)
+})
+
 
 </script>
-
+  
 <style scoped>
+.upload-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.upload-photo {
+    margin-bottom: 10px;
+}
+
 .upload-form-container {
     display: flex;
-    flex-direction: row;
-    justify-content: flex-start;
+    flex-direction: column;
     align-items: center;
-    margin: 20px 40px;
 }
 
-@media screen and (max-width: 768px) {
-    .upload-form-container {
-        flex-direction: column;
-        align-items: flex-start;
-    }
-
-
-    .custom-file-upload {
-        width: 80%;
-        margin-top: 10px;
-    }
-
-    .secondary {
-        margin-left: -2px;
-    }
-
-    .button-container {
-        margin-top: 10px;
-    }
+.input-container,
+.file-text-input,
+.radio-input {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 10px;
 }
+
 
 
 .custom-file-upload {
-    width: 30%;
+    width: 75%;
     height: 2rem;
     border: 1px solid #a8a5a5;
     border-radius: 10px;
-    font-family: 'Inter', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
+    font-family: 'Inter', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+        Oxygen, Ubuntu, Cantarell, 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+        sans-serif;
     font-size: 14px;
     padding: 0 15px;
-    margin-right: 20px;
+    margin-right: 15px;
     cursor: pointer;
-
-
 }
 
 .input-file {
     display: none;
 }
 
-.custom-file-upload {
-    display: inline-block;
-    justify-content: center;
+.custom-file-upload:focus {
+    outline: none;
+    border: 1px solid rgb(248, 57, 120);
 }
 
 .file-upload-text {
@@ -108,10 +133,24 @@ const color = ref('')
     height: 100%;
 }
 
+.button-container {
+    margin-left: 20px;
+}
 
+@media screen and (max-width: 768px) {
+    .input-container {
+        flex-direction: column;
+    }
 
-.custom-file-upload:focus {
-    outline: none;
-    border: 1px solid rgb(248, 57, 120);
+    .button-container {
+        margin-left: 0;
+    }
+
+    .custom-file-upload {
+        width: 50%;
+        margin-right: 5px;
+        margin-left: 10px;
+    }
 }
 </style>
+  
